19dd7e2ba85d13537ae6f35ba41776170fffdb9e
Thomas Lindauer <thomaslindauer@mail.com>
2018-12-12 23:23:26 +0100

    first commit

---
+++ resources/views/ouath-manager.blade.php
@@ -0,0 +1,26 @@
+<!doctype html>
+
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+
+        <title>Laravel</title>
+
+    </head>
+
+    <body>
+    
+        <div id="app">
+            <passport-clients></passport-clients>
+            <passport-authorized-clients></passport-authorized-clients>
+            <passport-personal-access-tokens></passport-personal-access-tokens>
+        </div>
+
+        <script type="text/javascript" src="/js/app.js"></script>
+
+    </body>
+
+</html>
---
+++ routes/api.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Http\Request;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::get('test', function () {
+
+    $test = array(
+        "dette er fra laravel over wifi LAN" => "heisann",
+        "bar" => "foo",
+    );
+
+    return $test;
+
+});
+
+Route::post('register', 'Auth\RegisterController@register');
+
+Route::middleware('auth:api')->get('/user', function (Request $request) {
+    return $request->user();
+});
